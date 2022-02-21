2a5c01c762779d7b8cd206f1a4c3eed3a71dac06
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-02-21 20:23:13 +0600

    Answer

---
+++ Assignment 1/Assignment 1.1/Q_11.py
@@ -0,0 +1,7 @@
+a_0 = int(input('Enter a integer number for arithmetic operation:'))
+b_0 = int(input('Enter a another integer number for arithmetic operation:'))
+print(a_0 + b_0, 'in Addition')
+print(a_0 - b_0, 'in Subtraction')
+print(a_0 * b_0, 'in Multiplication')
+print(a_0 // b_0, 'in Floor division')
+print(a_0 % b_0, 'in Modulus')
