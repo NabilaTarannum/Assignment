fe398da769a784609d2c1bd8a4f380687a79e902
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-02-22 23:15:48 +0600

    Answer

---
+++ Assignment/Assignment 1/Assignment 1.1/Q_22.py
@@ -0,0 +1,5 @@
+a = float(input('Enter the test number :'))
+b = float(input('Enter the test number :'))
+c = float(input('Enter the test number :'))
+d = a + b + c
+print('Total test number is', d)
