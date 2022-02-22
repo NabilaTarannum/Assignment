914403630112e1ef853e6a06b1417a570aac63f0
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-02-22 23:27:21 +0600

    Answer

---
+++ Assignment/Assignment 1/Assignment 1.1/Q_23.py
@@ -0,0 +1,14 @@
+a_1 = float(input('Enter the deposited money :'))
+a_2 = float(input('Enter the deposited money :'))
+a_3 = float(input('Enter the deposited money :'))
+a_4 = float(input('Enter the deposited money :'))
+a_5 = float(input('Enter the deposited money :'))
+a_6 = float(input('Enter the deposited money :'))
+a_7 = float(input('Enter the deposited money :'))
+a_8 = float(input('Enter the deposited money :'))
+a_9 = float(input('Enter the deposited money :'))
+a_10 = float(input('Enter the deposited money :'))
+
+a = a_1 + a_2 + a_3 + a_4 + a_5 + a_6 + a_7 + a_8 + a_9 + a_10
+
+print('Total deposited money is', a)
