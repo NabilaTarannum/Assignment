9ad6e537c84c95c5bd1d93e924b3c508bd7e37c7
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-02-25 12:56:06 +0600

    Answer

--- Assignment/Assignment 1/Assignment 1.1/Q_11.py
+++ Assignment/Assignment 1/Assignment 1.1/Q_11.py
@@ -1,5 +1,6 @@
 a_0 = int(input('Enter a integer number for arithmetic operation:'))
 b_0 = int(input('Enter a another integer number for arithmetic operation:'))
+
 print(a_0 + b_0, 'in Addition')
 print(a_0 - b_0, 'in Subtraction')
 print(a_0 * b_0, 'in Multiplication')
