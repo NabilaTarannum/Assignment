9f0b1874109939c8a6cacdeff5c958fab95d4029
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-02-25 13:02:02 +0600

    Answer

--- Assignment/Assignment 1/Assignment 1.1/Q_10.py
+++ Assignment/Assignment 1/Assignment 1.1/Q_10.py
@@ -1,3 +1,4 @@
 a = int(input('Enter a integer number for add operation:'))
 b = int(input('Enter a another integer number for add operation:'))
-print(a + b)
+
+print('The sum of number is', a + b)
