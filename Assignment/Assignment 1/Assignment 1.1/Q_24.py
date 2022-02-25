a307e10744dab2ccb4b94d6a801075e7e63801db
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-02-25 12:26:35 +0600

    Answer

--- Assignment/Assignment 1/Assignment 1.1/Q_24.py
+++ Assignment/Assignment 1/Assignment 1.1/Q_24.py
@@ -1,5 +1,11 @@
-x = int(input("Input a three digit numbers: "))
+x = int(input('Input a three digit numbers: '))
 x1 = (x // 10) // 10
 x2 = (x // 10) % 10
 x3 = (x % 10)
-print("The sum of digits in the number is", x1+x2+x3)
+print('The sum of digits in the number is', x1+x2+x3)
+
+# Another way 
+
+x = input('Input a three digit numbers: ')
+x_ = int(x[0]) + int(x[1]) + int(x[2])
+print('The sum of digits in the number is', x_)
