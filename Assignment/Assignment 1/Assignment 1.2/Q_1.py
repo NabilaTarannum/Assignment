6beaa04aaac3c1238ce5a1e622e4f4ecee52dfc6
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-02-25 12:33:11 +0600

    Answer

--- Assignment/Assignment 1/Assignment 1.2/Q_1.py
+++ Assignment/Assignment 1/Assignment 1.2/Q_1.py
@@ -1,1 +1,6 @@
+p1 = float(input('Enter first person meter :'))
+p2 = float(input('Enter second person meter :'))
+p3 = float(input('Enter third person meter :'))
+pm = ((p1 + p2 + p3) / 3)
 
+print('The average person\'s meter is', pm)
