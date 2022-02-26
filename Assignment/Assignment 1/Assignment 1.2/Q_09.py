66f62c72e5820a5b789fde0ab41266eece18f9e8
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-02-26 21:30:42 +0600

    Answer

--- Assignment/Assignment 1/Assignment 1.2/Q_09.py
+++ Assignment/Assignment 1/Assignment 1.2/Q_09.py
@@ -5,4 +5,5 @@ pi = 3.1428
 Volume = pi * r ** 2 * h
 surface = (2 * pi * r ** 2) + (2 * pi * r * h)
 
-print('The Volume of a Cylinder ', Volume, 'The Surface area of a Cylinder', surface)
+print('The Volume of a Cylinder', Volume)
+print('The Surface area of a Cylinder', surface)
