f2b5055eca1d488a9c99e935656c5eafdf48ac4d
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-02-26 14:05:43 +0600

    Answer

---
+++ Assignment/Assignment 1/Assignment 1.2/Q_04.py
@@ -0,0 +1,8 @@
+radius = float(input('Please Enter the Radius of a Sphere: '))
+PI = 3.1416
+
+Surface_Area = 4 * PI * radius ** 2
+Volume = (4 / 3) * PI * radius ** 3
+
+print("The Surface area of a Sphere is ", Surface_Area)
+print("The Volume of a Sphere is ", Volume)
