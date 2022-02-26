0a57e8465b28424934ea5b873dbbfff204c56bd7
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-02-27 00:44:50 +0600

    Answer

---
+++ Assignment/Assignment 1/Assignment 1.2/Q_14.py
@@ -0,0 +1,14 @@
+a_1 = int(input('Enter age of a human :'))
+a_2 = int(input('Enter age of a human :'))
+a_3 = int(input('Enter age of a human :'))
+a_4 = int(input('Enter age of a human :'))
+a_5 = int(input('Enter age of a human :'))
+a_6 = int(input('Enter age of a human :'))
+a_7 = int(input('Enter age of a human :'))
+a_8 = int(input('Enter age of a human :'))
+a_9 = int(input('Enter age of a human :'))
+a_10 = int(input('Enter age of a human :'))
+
+average = (a_1 + a_2 + a_3 + a_4 + a_5 + a_6 + a_7 + a_8 + a_9 + a_10) / 10
+
+print('Average age of human is', average)
