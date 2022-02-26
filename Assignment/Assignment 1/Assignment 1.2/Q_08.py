84dfbd728b5c71ea791432bb8141d0f533490b70
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-02-26 19:23:47 +0600

    Answer

---
+++ Assignment/Assignment 1/Assignment 1.2/Q_08.py
@@ -0,0 +1,7 @@
+l = float(input('Please Enter the Length of any Side of a Cube: '))
+
+surface = 6 * (l ** 2)
+Volume = l ** 3
+
+print("Surface Area of Cube is", surface)
+print("Volume of cube is", Volume)
