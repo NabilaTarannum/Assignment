c5303501a546a98dbfaae90a5a88a3939acfb07e
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2022-02-26 21:29:20 +0600

    Answer

--- Assignment/Assignment 1/Assignment 1.2/Q_09.py
+++ Assignment/Assignment 1/Assignment 1.2/Q_09.py
@@ -1,8 +1,8 @@
-r = int(input('Enter :'))
-h = int(input('Enter :'))
+r = int(input('Enter the Radius of a Cylinder :'))
+h = int(input('Enter the Height of a Cylinder :'))
 
 pi = 3.1428
 Volume = pi * r ** 2 * h
 surface = (2 * pi * r ** 2) + (2 * pi * r * h)
 
-print(Volume, surface)
+print('The Volume of a Cylinder ', Volume, 'The Surface area of a Cylinder', surface)
